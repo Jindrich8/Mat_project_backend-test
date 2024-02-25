2bec4a7971f86a59b3cf09e98c35fb5c8699953e
Jindřich Svatoň <jindrich.svaton@gmail.com>
2024-02-26 00:19:55 +0100

    Add auth middleware protection to routes and add myers diff algorithm

--- mat-project-laravel-test/routes/api.php
+++ mat-project-laravel-test/routes/api.php
@@ -22,88 +22,116 @@ use App\Utils\RouteUtils;
 |
 */
 
-// RouteUtils::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->group(function () {
+
+    RouteUtils::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    #region Teacher
+
+    RouteUtils::post(
+        '/task/create',
+        fn (Request $request) =>
+        TaskController::construct()
+            ->store($request)
+    );
+
+    RouteUtils::put(
+        '/task/{id}/update',
+        fn (Request $request, string $id) =>
+        TaskController::construct()
+            ->update($request, RequestHelper::translateId($id))
+    );
+
+    RouteUtils::delete(
+        '/task/{id}/delete',
+        fn (Request $request, string $id) =>
+        TaskController::construct()
+            ->delete($request, RequestHelper::translateId($id))
+    );
+
+    RouteUtils::get(
+        '/task/create_info',
+        fn (Request $request) =>
+        TaskCreateInfoController::construct()
+            ->getCreateInfo($request)
+    );
+
+    RouteUtils::get(
+        '/my_task/list',
+        fn (Request $request) =>
+        TaskController::construct()
+            ->myList($request)
+    );
+
+    RouteUtils::get(
+        '/my_task/{id}/detail',
+        fn (Request $request, string $id) =>
+        TaskController::construct()
+            ->myDetail($request, RequestHelper::translateId($id))
+    );
 
 
-#region Teacher
+    #endregion Teacher
 
-RouteUtils::post('/task/create',fn(Request $request) =>
+    #region User
+
+    RouteUtils::get(
+        '/review/list',
+        fn (Request $request) =>
+        TaskReviewController::construct()
+            ->list($request)
+    );
+
+    RouteUtils::get(
+        '/review/{id}/detail',
+        fn (Request $request, string $id) =>
+        TaskReviewController::construct()
+            ->detail($request, RequestHelper::translateId($id))
+    );
+
+    RouteUtils::get(
+        '/review/{id}/get',
+        fn (Request $request, string $id) =>
+        TaskReviewController::construct()
+            ->get($request, RequestHelper::translateId($id))
+    );
+
+    RouteUtils::get(
+        '/review/{id}/delete',
+        fn (Request $request, string $id) =>
+        TaskReviewController::construct()
+            ->delete($request, RequestHelper::translateId($id))
+    );
+
+    #endregion User
+});
+
+RouteUtils::get(
+    '/task/{id}/detail',
+    fn (Request $request, string $id) =>
     TaskController::construct()
-    ->store($request)
+        ->detail($request, RequestHelper::translateId($id))
 );
 
-RouteUtils::put('/task/{id}/update',fn(Request $request,string $id) =>
+RouteUtils::get(
+    '/task/{id}/take',
+    fn (Request $request, string $id) =>
     TaskController::construct()
-    ->update($request,RequestHelper::translateId($id))
+        ->take($request, RequestHelper::translateId($id))
 );
 
-RouteUtils::delete('/task/{id}/delete',fn(Request $request,string $id)=>
+RouteUtils::post(
+    '/task/{id}/evaluate',
+    fn (Request $request, string $id) =>
     TaskController::construct()
-    ->delete($request,RequestHelper::translateId($id))
+        ->evaluate($request, RequestHelper::translateId($id))
 );
 
-RouteUtils::get('/task/create_info',fn(Request $request) =>
-    TaskCreateInfoController::construct()
-    ->getCreateInfo($request)
-);
-
-RouteUtils::get('/my_task/list',fn(Request $request) =>
+RouteUtils::get(
+    '/task/list',
+    fn (Request $request) =>
     TaskController::construct()
-    ->myList($request)
-);
-
-RouteUtils::get('/my_task/{id}/detail',fn(Request $request,string $id)=>
-    TaskController::construct()
-    ->myDetail($request,RequestHelper::translateId($id))
-);
-
-
-#endregion Teacher
-
-#region User
-
-RouteUtils::get('/review/list',fn(Request $request) =>
-    TaskReviewController::construct()
-    ->list($request)
-);
-
-RouteUtils::get('/review/{id}/detail',fn(Request $request,string $id) =>
-    TaskReviewController::construct()
-    ->detail($request,RequestHelper::translateId($id))
-);
-
-RouteUtils::get('/review/{id}/get',fn(Request $request,string $id) =>
-    TaskReviewController::construct()
-    ->get($request,RequestHelper::translateId($id))
-);
-
-RouteUtils::get('/review/{id}/delete',fn(Request $request,string $id) =>
-    TaskReviewController::construct()
-    ->delete($request,RequestHelper::translateId($id))
-);
-
-#endregion User
-
-
-
-RouteUtils::get('/task/{id}/detail',fn(Request $request,string $id) =>
-    TaskController::construct()
-    ->detail($request,RequestHelper::translateId($id))
-);
-
-RouteUtils::get('/task/{id}/take',fn(Request $request,string $id) =>
-    TaskController::construct()
-    ->take($request,RequestHelper::translateId($id))
-);
-
-RouteUtils::post('/task/{id}/evaluate',fn(Request $request,string $id)=>
-    TaskController::construct()
-    ->evaluate($request,RequestHelper::translateId($id))
-);
-
-RouteUtils::get('/task/list',fn(Request $request) =>
-    TaskController::construct()
-    ->list($request)
+        ->list($request)
 );
