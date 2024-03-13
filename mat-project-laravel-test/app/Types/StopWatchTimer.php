b56921be977069a8ea4bc92cc035ccdf16282ae8
Jindřich Svatoň <jindrich.svaton@gmail.com>
2024-03-13 02:27:55 +0100

    Fix StopWatchTimer units (micro seconds to seconds) and add start log entry

--- mat-project-laravel-test/app/Types/StopWatchTimer.php
+++ mat-project-laravel-test/app/Types/StopWatchTimer.php
@@ -13,11 +13,12 @@ namespace App\Types {
          * @return TRet
          */
         public static function run(string $name,callable $action,?callable $transformResForLog = null){
+            DebugLogger::logger()->performance("START '$name'");
             $start = microtime(true);
             $res = $action();
             $end = microtime(true);
             $value = $transformResForLog ? $transformResForLog($res) : $res;
-            DebugLogger::performance($end-$start,SIUnitPrefixEnum::MICRO->value.'s',$name,$value);
+            DebugLogger::performance($end-$start,'s',$name,$value);
             return $res;
         }
     }
